00c555158689b0bc5a736d731d77e66b548a0e56
huangyichao0916 <939121836@qq.com>
2021-03-15 23:02:55 +0800

    263. 丑数

---
+++ src/p263/P263.java
@@ -0,0 +1,24 @@
+package p263;
+
+public class P263 {
+    public static void main(String[] args) {
+    }
+}
+
+class Solution {
+    public boolean isUgly(int n) {
+        if (n < 1){
+            return false;
+        }
+        while (n % 2 == 0){
+            n = n / 2;
+        }
+        while (n % 3 == 0){
+            n = n / 3;
+        }
+        while (n % 5 == 0){
+            n = n / 5;
+        }
+        return n == 1;
+    }
+}
