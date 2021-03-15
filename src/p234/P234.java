a7d397335f16b48a359bc5bddfce45c5afe3fc37
huangyichao0916 <939121836@qq.com>
2021-03-15 22:54:32 +0800

    234. 回文链表

---
+++ src/p234/P234.java
@@ -0,0 +1,27 @@
+package p234;
+
+import java.util.ArrayList;
+
+public class P234 {
+    public static void main(String[] args) {
+        ArrayList arr1 = new ArrayList();
+//        arr1.get()
+    }
+}
+
+//class Solution {
+//    public boolean isPalindrome(ListNode head) {
+//        ArrayList<Integer> al1 = new ArrayList<Integer>();
+//        while(head != null){
+//            al1.add(head.val);
+//            head = head.next;
+//        }
+//        int len = al1.size();
+//        for(int i = 0,j = len - 1; j >= i; i++, j--){
+//            if(al1.get(i) != al1.get(j)){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
